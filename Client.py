2741751adc94724e1e328fddc4686494a190e527
Tintti <teme.eloranta@gmail.com>
2023-03-19 11:58:35 +0200

    Kaik toimii

--- Client.py
+++ Client.py
@@ -7,7 +7,7 @@ proxy = xmlrpc.client.ServerProxy("http://localhost:5001/")
 #nro2 = 20
 
 def start():
-    #Start client loop, ask for a note or quit
+    #Start client loop, ask for a new note, look for old note or quit
     print("Hello, welcome to note-machine")
     while True:
         print("******************")
@@ -21,18 +21,26 @@ def start():
             txt = input("Give note text: ")
             print("Sending new note")
             time = str(datetime.now())
-            print(time)
-            print(topic2, note, txt)
+            #print(time)
+            #print(topic2, note, txt)
             #result = proxy.sendtext(topic, note, txt, time)
             result = proxy.sendtext(topic2, note, txt, time)
-            print(f"Result is: {result}")
+            print(f"Created new note on data: {result}")
             #print("This is the note: ",result[0],result[1])
         if choice == 2:
             header = input("Give note topic: ")
-            print("Looking for note on given topic")
+            print("Looking for note(s) on given topic")
+            result = proxy.lookfornote(header)
+            #Check if found results
+            if not result:
+                print("No notes found")
+            else:
+                print("Note(s) found:")
+                for note in result:
+                    print(note)
 
         if choice == 3:
-            print("Thanks for using")
+            print("Thanks for using, exiting")
             break
 start()
 
--- Server.py
+++ Server.py
@@ -4,8 +4,7 @@ import xml.etree.ElementTree as ET
 
 def test(num1, num2):
     return num1 + num2
-
-#def sendtext(topic2, note, text, time):
+#
 def sendtext(topic2, note, text, time):
     tree = ET.parse('testi.xml')
     root = tree.getroot()
@@ -20,13 +19,13 @@ def sendtext(topic2, note, text, time):
             #print("No topic found")
     for topic in root.findall("topic"):
         tname = topic.get("name")
-        print("name is ",tname)
+        #print("name is ",tname)
         #if match to old topic, append other data to it
         if tname == topic2:
             
             print("Match on topic")
             check = True
-            newnote = ET.SubElement(topic, "note")
+            newnote = ET.SubElement(topic, "note", name=note)
             newtext = ET.SubElement(newnote, "text")
             newtext.text = text
             newtim = ET.SubElement(newnote, "timestamp")
@@ -65,15 +64,49 @@ def sendtext(topic2, note, text, time):
 
     return topic2,text,note,time
     #return
+#Search for note
+def lookfornote(topic2):
+    notelist = []
+    tree = ET.parse('testi.xml')
+    root = tree.getroot()
+    print("Looking")
+    #If matching topic found
+    for topic in root.findall("topic"):
+        tname = topic.get("name")
+        if tname == topic2:
+            print("Match found")
+            #Look through notes in topic, saving note name to list
+            for noot in topic.findall("note"):
+                nname = noot.get("name")
+                #print(nname)
+                notelist.append(nname)
+                #look through text in note and, save text to list
+                for txti in noot.findall("text"):
+                    ntext = txti.text
+                    #print("Tässä o teksti", ntext)
+                    notelist.append(ntext)
+            print(topic)
+            #ET.dump(topic)
+            return(notelist)
+        else:
+            print("No match found")
+            return(notelist)
+    
 
-
+    
 #start and serve the server, and functions
 def start():
     port = 5001
-    print("Server starting at")
+    print("Server starting ")
     server = SimpleXMLRPCServer(("localhost", port))
     server.register_function(test,"test")
     server.register_function(sendtext,"sendtext")
-    server.serve_forever()
+    server.register_function(lookfornote, "lookfornote")
+    try:
+        server.serve_forever()
+    #Clean mesg if ctrl + c
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        exit
     
 start()
